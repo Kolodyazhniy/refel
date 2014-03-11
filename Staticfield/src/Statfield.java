7c927eaf9cded1567b53db547ee3624952a9737d
alexwan <alexwan1122@gmail.com>
2014-03-11 11:15:26 +0200

    1

---
+++ Staticfield/src/Statfield.java
@@ -0,0 +1,28 @@
+
+public class Statfield {
+
+			
+		private int mark = 3;
+		public static int coeff = 5;
+		public double getResult() {
+		return (double)coeff*mark/100;
+		}
+		public static void setCoeffFloat(float c) {
+		coeff = (int) ((int) coeff*c);
+		}
+		public void setMark(int mark) {
+		this.mark = mark;
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+
